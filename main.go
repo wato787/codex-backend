bc1105aa7cd78fb6fb40e1b7cfc1194d2a14abe5
wato787 <wato787@icloud.com>
2025-04-05 20:17:04 +0900

    db接続追加

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"log"
+
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
 	"github.com/wato787/app/controller"
+	"github.com/wato787/app/models"
+	"github.com/wato787/db"
 	"github.com/wato787/docs"
 )
 
 func main() {
+
+	// DB接続
+	SetupDB()
+
+	// ルーターのセットアップ
 	route := SetupRoutes()
 	docs.SwaggerInfo.BasePath = "/api"
 	api := route.Group("/api")
@@ -18,10 +27,19 @@ func main() {
 	}
 	route.GET("/doc/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 	route.Run(":8080")
+	
 }
 
 func SetupRoutes() *gin.Engine {
 	route := gin.Default()
 	route.GET("/", controller.HelloWorld)
 	return route
+}
+
+func SetupDB() {
+	if err := db.Connect(db.DefaultConfig()); err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	db.DB.AutoMigrate(&models.User{})
 }
