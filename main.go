8972dc6eacf2875c6e4217586c2200469d7c1bbb
wato787 <wato787@icloud.com>
2025-04-07 22:23:53 +0900

    不要コメント削除

--- main.go
+++ main.go
@@ -14,25 +14,7 @@ import (
 	"github.com/wato787/docs"
 )
 
-// @title           Codex API
-// @version         1.0
-// @description     Go言語とGORMで構築されたAPI
-// @termsOfService  http://swagger.io/terms/
 
-// @contact.name   API Support
-// @contact.url    http://www.example.com/support
-// @contact.email  support@example.com
-
-// @license.name  MIT
-// @license.url   https://opensource.org/licenses/MIT
-
-// @host      localhost:8080
-// @BasePath  /api
-
-// @securityDefinitions.apikey BearerAuth
-// @in header
-// @name Authorization
-// @description Type "Bearer" followed by a space and JWT token.
 func main() {
 	// DB接続
 	SetupDB()
