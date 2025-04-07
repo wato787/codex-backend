bf43fcc2dc0f524a0042bad89c6960a9feea2fd6
wato787 <wato787@icloud.com>
2025-04-07 16:45:46 +0900

    ユーザーモデル拡張

--- app/model/user.go
+++ app/model/user.go
@@ -1,8 +1,30 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
+)
 
 type User struct {
 	*gorm.Model
-	Name string `json:"name" gorm:"type:varchar(100);not null;unique"`
+	Email    string `json:"email" gorm:"type:varchar(100);not null;unique"`
+	Password string `json:"-" gorm:"type:varchar(255);not null"`
+}
+
+// BeforeSave はユーザーが保存される前にパスワードをハッシュ化する
+func (u *User) BeforeSave(tx *gorm.DB) error {
+	if u.Password != "" {
+		hashedPassword, err := bcrypt.GenerateFromPassword([]byte(u.Password), bcrypt.DefaultCost)
+		if err != nil {
+			return err
+		}
+		u.Password = string(hashedPassword)
+	}
+	return nil
+}
+
+// CheckPassword はパスワードが正しいかどうかを確認する
+func (u *User) CheckPassword(password string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(u.Password), []byte(password))
+	return err == nil
 }
